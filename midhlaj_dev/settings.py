5c2e505934c67d1a3bed78d99db211274117b374
Muhammad Midhlaj <midhlajmk765@gmail.com>
2026-09-19 00:43:16 +0530

    Update edit project page

--- midhlaj_dev/settings.py
+++ midhlaj_dev/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = 'django-insecure-h_4r*x5cx%kxtg@3j*eb29qsn-1s82&+2ljmb%12btdt+5r^0g
 DEBUG = True
 
 ALLOWED_HOSTS = [
+ "midhlajmk.pythonanywhere.com",
 ]
-
 
 # Application definition
 
